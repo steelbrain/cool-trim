9cc769d14980ff68cf05876f1225783f9d160828
steelbrain <de.steelbrain@gmail.com>
2016-03-10 16:02:07 -0700

    :new: Reimplement whitespace trimming

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@
 /* @flow */
 
 const trimNewLines = require('trim-newlines')
-const stripIndent = require('strip-indent')
+const indentionRegex = /^ +/
+const allEmpty = /^ +$/
 
 function trim(subject: string): string {
-  return stripIndent(trimNewLines(subject))
+  const toReturn = []
+  const chunks = subject.split(/\r\n|\n/g)
+  let leastIndention = Infinity
+
+  // Detect indention
+  for (let i = 0; i < chunks.length; ++i) {
+    const chunk = chunks[i]
+    const match = chunk.match(indentionRegex)
+    const indention = match ? match[0].length : 0
+    if (chunk.length && indention < leastIndention && !allEmpty.test(chunk)) {
+      leastIndention = indention
+    }
+  }
+
+  if (leastIndention !== Infinity) {
+    for (let i = 0; i < chunks.length; ++i) {
+      const chunk = chunks[i]
+      toReturn.push(chunk.substr(leastIndention))
+    }
+
+    return trimNewLines(toReturn.join('\n'))
+  }
+
+  return trimNewLines(chunks.join('\n'))
 }
 
 module.exports = trim
